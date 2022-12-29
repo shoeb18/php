9946cfae2c66ed41fbb98a8a327e76df53ca5e43
shoeb khan <shoebkhan0197@gmail.com>
2022-12-30 00:23:48 +0530

    Added global local scope program

---
+++ 20_variable_scope.php
@@ -0,0 +1,20 @@
+<?php
+echo "Welcome to the local and global scope of variables <br>";
+
+$a = 34; // Global variable
+
+// global variable score are outside the functions in php
+
+function printValue(){
+    // $a = 90; // Local variable
+    global $a; // Give me the access to global variable a
+    $a = 100;
+    echo "The value of your variable is : $a <br>";
+}
+
+echo $a;
+echo "<br>";
+printValue();
+
+
+?>
